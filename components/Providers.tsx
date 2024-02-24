3d0ac88ff43e81c110a24ab90b7664ef7537f555
ZENHOM <zenhom.dev@gmail.com>
2024-02-24 08:45:01 +0200

    fix: trpc request url in production

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -5,11 +5,15 @@ import { httpBatchLink } from '@trpc/client'
 import React, { useState } from 'react'
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
+    const url = process.env.VERCEL_URL
+        ? `https://${process.env.VERCEL_URL}/api/trpc`
+        : 'http://localhost:3000/api/trpc';
+
     const [queryClient] = useState(() => new QueryClient())
     const [trpcClient] = useState(() => trpc.createClient({
         links: [
             httpBatchLink({
-                url: "http://localhost:3000/api/trpc"
+                url
             })
         ]
     }))
