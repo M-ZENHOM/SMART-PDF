a35860bd9bd7054b96c978cb0d3e20c07eafafd3
ZENHOM <zenhom.dev@gmail.com>
2024-03-03 16:17:25 +0200

    chore: change production url domain to right one

--- lib/utils.ts
+++ lib/utils.ts
@@ -18,7 +18,7 @@ export function formatTime(timeStr: string): string {
 export function absoluteUrl(path: string) {
   if (typeof window !== 'undefined') return path
   if (process.env.VERCEL_URL)
-    return `https://${process.env.VERCEL_URL}${path}`
+    return `${process.env.NEXT_PUBLIC_URL}${path}`
   return `http://localhost:${process.env.PORT ?? 3000
     }${path}`
 }
